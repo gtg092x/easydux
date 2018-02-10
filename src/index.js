c342ffa1c91826f76c90215650115ed155b896c7
gtg092x <mdrake@mediadrake.com>
2018-02-10 13:24:40 -0800

    use path

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+import isArray from 'lodash/isArray';
+import { setCopy as setCopyObj } from './object';
+
+export const setCopy = (target, path, value) =>
+  setCopyObj(target, isArray(path) ? path : path.split('.'), value);
+
 export value from './value';
 export array from './array';
 export object from './object';
-export * from './object';
